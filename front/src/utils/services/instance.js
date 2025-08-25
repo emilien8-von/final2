031d98927003445c5763897e47f7a88492502de7
Emilien <emilienmabonzo123@gmail.com>
2025-08-25 16:53:35 +0200

    Fix instance

--- front/src/utils/services/instance.js
+++ front/src/utils/services/instance.js
@@ -1,13 +1,11 @@
 // Dans front/src/utils/services/instance.js
 import axios from "axios";
 
-// LA CORRECTION N°1 : On lit la variable d'environnement
-// En local, Vite prendra la valeur de .env.local
-// Sur Vercel, Vite prendra la valeur que vous avez configurée dans les settings
-const INSTANCE_URL = import.meta.env.VITE_INSTANCE_URL;
+// LA CORRECTION : On utilise le bon nom de variable d'environnement
+const API_URL = import.meta.env.VITE_API_URL;
+
 const INSTANCE = axios.create({
-    // LA CORRECTION N°2 : On corrige la faute de frappe "baseUrl" -> "baseURL"
-    baseURL: INSTANCE_URL, 
+    baseURL: API_URL, 
     withCredentials: true,
     headers: {
         'Content-Type': "application/json"
